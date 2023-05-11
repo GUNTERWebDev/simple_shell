7ac06fa6380eb51997263a4e75b1b569b8789ed4
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-11 08:02:40 -0700

    test

--- test.c
+++ test.c
@@ -5,21 +5,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_ARGS 16  // maximum number of arguments
+#define MAX_ARGS 16  
 
 /**
  * Tokenizes the input string into an array of arguments.
  * Returns the number of arguments.
  */
 int tokenize(char *input, char *args[]) {
-    // tokenize the input string
     int i = 0;
     char *token = strtok(input, " ");
     while (token != NULL && i < MAX_ARGS) {
         args[i++] = token;
         token = strtok(NULL, " ");
     }
-    args[i] = NULL;  // set the last argument to NULL
+    args[i] = NULL;
     return i;
 }
 
@@ -29,44 +28,44 @@ int main(void) {
     char *args[MAX_ARGS+1];
     char *env[] = {NULL};
     ssize_t line_len;
+    int argc, status;
+    pid_t pid;
+
     while (true) {
         printf("$ ");
 
         line_len = getline(&line, &len, stdin);
-        if (line_len == -1)  // end of file or error
+        if (line_len == -1)
             break;
 
-        if (strcmp(line, "exit\n") == 0)  // exit the shell
+        if (strcmp(line, "exit\n") == 0)
             break;
 
-        // remove trailing newline
         if (line[line_len-1] == '\n')
             line[line_len-1] = '\0';
 
-        int argc = tokenize(line, args);  // tokenize the input
+        argc = tokenize(line, args);
         if (argc > 0) {
-            // build a new argument list with full path to the executable
             char path[128] = "/usr/bin/";
             strcat(path, args[0]);
             args[0] = path;
 
-            pid_t pid = fork();
-            if (pid == -1) {  // fork failed
+            pid = fork();
+            if (pid == -1) {
                 perror("fork");
-            } else if (pid == 0) {  // child process
-                if (execve(args[0], args, env) == -1) {  // execute the command
+            } else if (pid == 0) {
+                if (execve(args[0], args, env) == -1) {
                     perror(args[0]);
                     exit(EXIT_FAILURE);
                 }
-            } else {  // parent process
-                int status;
-                if (waitpid(pid, &status, 0) == -1) {  // wait for the child to finish
+            } else {
+                if (waitpid(pid, &status, 0) == -1) {
                     perror("waitpid");
                 }
             }
         }
     }
 
-return (0);
+    free(line);
+    return 0;
 }
-
