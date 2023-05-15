1e3d05f2b1eac26d443008fbd6d832156665b7da
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-15 09:57:30 -0700

    main

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 16
+
+int tokenize(char *input, char *args[]);
+
+#endif /* MAIN_H */
