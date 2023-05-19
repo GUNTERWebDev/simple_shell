9613a99e4d82c2cc1e3ee73d721890e44f3be8e6
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-19 09:49:05 -0700

    main

--- main.h
+++ main.h
@@ -9,7 +9,10 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 16
+
 extern char **environ;
 int tokenize(char *input, char *args[]);
-
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 #endif /* MAIN_H */
