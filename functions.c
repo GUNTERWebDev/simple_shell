97e7389de9b93297c5330122e0377cc257f86d07
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-19 12:24:18 -0700

    function

--- functions.c
+++ functions.c
@@ -61,7 +61,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
-#include "main.h"
 /**
  * _atoi - convert a string to an integer
  * @s: given string
@@ -96,4 +95,31 @@ int _atoi(char *s)
 	}
 	n *= sign;
 	return (n);
+}
+/**
+ * print_number - print a given number using _putchar
+ * @n: number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n);
+	}
+	else if (n > 9)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	else
+		 _putchar(n + '0');
 }
