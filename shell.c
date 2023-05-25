7f7cc90ea358b3b528572456ed1cbbe2f78283a4
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-24 17:09:07 -0700

    shell

--- shell.c
+++ shell.c
@@ -109,7 +109,8 @@ int main(__attribute__((unused))int ac, char **av)
 		line_len = getline(&line, &len, stdin);
 		if (line_len == -1)
 		{
-			write(STDOUT_FILENO, "\n", 1);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
 		if (_strcmp(line, "exit") == 0)
