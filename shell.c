fff57f5fe970aa9da2b9fd0a94a46ba0554a8752
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-25 08:49:06 -0700

    simple shell

--- shell.c
+++ shell.c
@@ -108,7 +108,6 @@ int main(__attribute__((unused))int ac, char **av)
 		if (line_len == -1)
 		{
 			write(1, "Exiting shell....\n", 18);
-			free(line);
 			return (-1);
 		}
 		if (_strcmp(line, "exit") == 0)
